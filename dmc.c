c20027b189fca1dcb858ed7bab5ad15ed55eb254
byllgrim <robin.a.t.pedersen@gmail.com>
2017-04-18 07:40:04 +0200

    get user input and execute it

--- dmc.c
+++ dmc.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 int
 main(void)
 {
+	char *s;
+
+	s = calloc(BUFSIZ + 1, sizeof(*s));
+	for (;;) {
+		fgets(s, BUFSIZ, stdin); /* TODO check return? */
+		/* TODO getline(s, BUFSIZ) which does checking */
+		s[strlen(s) - 1] = '\0';
+		execlp(s, s, (void *)0);
+	}
+
 	return 0;
 }
