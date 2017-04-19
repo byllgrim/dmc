88857c0ae6b8e7b577ef52fc5c17090c30cd836f
byllgrim <robin.a.t.pedersen@gmail.com>
2017-04-19 08:49:58 +0200

    refactor main

--- dmc.c
+++ dmc.c
@@ -3,17 +3,34 @@
 #include <string.h>
 #include <unistd.h>
 
+char *
+get_input(void)
+{
+	char *s;
+
+	s = calloc(BUFSIZ + 1, sizeof(*s));
+	fgets(s, BUFSIZ, stdin); /* TODO check return */
+	s[strlen(s) - 1] = '\0';
+
+	return s;
+}
+
+void
+execute(char *s) /* TODO tokenize s (before?) */
+{
+	/* TODO fork */
+	execlp(s, s, (void *)0);
+}
+
 int
 main(void)
 {
 	char *s;
 
-	s = calloc(BUFSIZ + 1, sizeof(*s));
 	for (;;) {
-		fgets(s, BUFSIZ, stdin); /* TODO check return? */
-		/* TODO getline(s, BUFSIZ) which does checking */
-		s[strlen(s) - 1] = '\0';
-		execlp(s, s, (void *)0);
+		s = get_input();
+		execute(s);
+		free(s);
 	}
 
 	return 0;
